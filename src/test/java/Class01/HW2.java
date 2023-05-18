00019f2b4a36065f0d7501b948db48a4f1c36799
cborja09@icloud.com <cborja09@icloud.com>
2023-05-17 22:24:07 -0700

    Updated Commit

--- src/test/java/Class01/HW2.java
+++ src/test/java/Class01/HW2.java
@@ -1,5 +1,9 @@
 package Class01;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HW2 {
     /*
     Note: use name or id as locators
@@ -8,6 +12,22 @@ public class HW2 {
     Enter the password "test"
     get the title of the webPage and confirm that it is  "Web Orders Login".
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+        driver.manage().window().maximize();
+        driver.findElement(By.name("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+        String title = driver.getTitle();
+        System.out.println(title);
+        Thread.sleep(5000);
+        if(title.equals("Web Orders Login")){
+            System.out.println("The title is correct.");
+        }else{
+            System.out.println("The title is incorrect.");
+        }
+        driver.quit();
+
     }
 }
