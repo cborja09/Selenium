77f642aaca23c2efdc71dd7a6837c29853f0e1a6
cborja09@icloud.com <cborja09@icloud.com>
2023-05-17 22:03:20 -0700

    Updated Commit

--- src/test/java/Class01/HW2.java
+++ src/test/java/Class01/HW2.java
@@ -2,6 +2,7 @@ package Class01;
 
 public class HW2 {
     /*
+    Note: use name or id as locators
     Navigate to "http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx"
     Enter the username "Tester"
     Enter the password "test"
