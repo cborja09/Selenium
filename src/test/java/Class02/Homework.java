829d2862e2c8eb5c665f9685f3e5e67e2f458903
cborja09@icloud.com <cborja09@icloud.com>
2023-05-23 13:00:27 -0700

    Updated Commit

---
+++ src/test/java/Class02/Homework.java
@@ -0,0 +1,14 @@
+package Class02;
+
+public class Homework {
+    // HW  (only use XPATH)
+    //navigate to fb.com
+    //click on create new account
+    //fill up all the text boxes
+    //close the popup
+    //close the browser
+
+    // Note : in order for ur code to work
+    //u have to put Thread.sleep(2000) after clicking on Create new account
+
+}
