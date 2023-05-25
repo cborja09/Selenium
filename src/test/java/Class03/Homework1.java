831a3a8b922f01188642122116efac12292c8c52
cborja09@icloud.com <cborja09@icloud.com>
2023-05-25 13:49:41 -0700

    Updated Commit

--- src/test/java/Class03/Homework1.java
+++ src/test/java/Class03/Homework1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework1 {
     //Open Chrome browser
-    //Go to: “http://practice.syntaxtechs.net/input-form-demo.php%E2%80%9D
+    //Go to: “http://practice.syntaxtechs.net/input-form-demo.php%E2%80%9D"
     //Fill in the complete form
 
     public static void main(String[] args) throws InterruptedException {
