30c3ebefe1809fccb06553fe26aa034ecbad74b7
cborja09@icloud.com <cborja09@icloud.com>
2023-05-24 19:07:51 -0700

    Updated Commit

--- src/test/java/Class03/Homework2.java
+++ src/test/java/Class03/Homework2.java
@@ -1,4 +1,9 @@
 package Class03;
 
 public class Homework2 {
+    //Go to “http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login”
+    //Enter valid username
+    //Leave password field empty
+    //Click on login button
+    //Verify error message with text “Password cannot be empty” is displayed.
 }
