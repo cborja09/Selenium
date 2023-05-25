ea48a6c632091d0b25e2381a9f968d0b5e90094f
cborja09@icloud.com <cborja09@icloud.com>
2023-05-25 13:46:05 -0700

    Updated Commit

--- src/test/java/Class03/Homework2.java
+++ src/test/java/Class03/Homework2.java
@@ -1,6 +1,8 @@
 package Class03;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework2 {
@@ -10,13 +12,21 @@ public class Homework2 {
     //Click on login button
     //Verify error message with text “Password cannot be empty” is displayed.
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
 
         driver.manage().window().maximize();
+
+       WebElement userName = driver.findElement(By.xpath("//input[@name='txtUsername']"));
+       userName.sendKeys("Admin");
+
+       WebElement login = driver.findElement(By.xpath("//input[@id='btnLogin']"));
+       Thread.sleep(1000);
+       login.click();
+
 
     }
 }
