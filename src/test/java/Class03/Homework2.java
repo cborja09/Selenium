3b7c73f50c7b41b7258ffec7cb4081857d4b623c
cborja09@icloud.com <cborja09@icloud.com>
2023-05-25 13:50:33 -0700

    Updated Commit

--- src/test/java/Class03/Homework2.java
+++ src/test/java/Class03/Homework2.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homework2 {
-    //Go to “http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login”
+    //Go to: “http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login”
     //Enter valid username
     //Leave password field empty
     //Click on login button
-    //Verify error message with text “Password cannot be empty” is displayed.
+    //Verify error message with text “Password cannot be empty” is displayed
 
     public static void main(String[] args) throws InterruptedException {
 
