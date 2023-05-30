440f89357b5a728f760b4cd1da2d1f2e6a8df81e
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 19:32:18 -0700

    Updated Commit

--- src/test/java/Class04/DropDowns.java
+++ src/test/java/Class04/DropDowns.java
@@ -3,6 +3,7 @@ package Class04;
 import Utlis.CommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class DropDowns extends CommonMethods {
     public static void main(String[] args) throws InterruptedException {
@@ -18,7 +19,15 @@ public class DropDowns extends CommonMethods {
         //and the code takes some time to appear in the DOM for first name
         //so we must add a sleep here
         Thread.sleep(2000);
-        //find the element associated with the dropdown
+        // 1. find the element associated with the dropdown
         WebElement days = driver.findElement(By.xpath("//select[@id='day']"));
+        // 2. create an object of the select class and pass in the parameters the element dropdown
+        Select sel = new Select(days);
+        // 3. use method provided in select class to select the option that you desire
+        sel.selectByVisibleText("8");
+        //for observation
+        Thread.sleep(2000);
+        //another method
+        sel.selectByValue("26");
     }
 }
