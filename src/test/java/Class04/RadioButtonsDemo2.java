1b39e219f65c9d082fa6eab6a81075a6106afffd
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 17:22:36 -0700

    Updated Commit

---
+++ src/test/java/Class04/RadioButtonsDemo2.java
@@ -0,0 +1,12 @@
+package Class04;
+
+import Utlis.CommonMethods;
+
+public class RadioButtonsDemo2 extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://practice.syntaxtechs.net/basic-radiobutton-demo.php";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+    }
+}
