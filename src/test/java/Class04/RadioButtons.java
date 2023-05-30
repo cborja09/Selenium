9260261c38a28019bc7864fdcb0f1f402b3efd73
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 17:05:25 -0700

    Updated Commit

--- src/test/java/Class04/RadioButtons.java
+++ src/test/java/Class04/RadioButtons.java
@@ -2,12 +2,31 @@ package Class04;
 
 import Utlis.CommonMethods;
 import io.netty.util.internal.logging.CommonsLoggerFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import javax.sql.CommonDataSource;
 
 public class RadioButtons extends CommonMethods {
     public static void main(String[] args) {
-        openBrowserAndLaunchApplication("http://practice.syntaxtechs.net/basic-radiobutton-demo.php","chrome");
-    }
+        String url="http://practice.syntaxtechs.net/basic-radiobutton-demo.php";
+        String browser="chrome";
+        openBrowserAndLaunchApplication(url,browser);
 
+        //find the radio button male and click on it
+        WebElement r1 = driver.findElement(By.xpath("//input[@value='Male' and @name='optradio']"));
+        r1.click();
+
+        //check if the radio button r1 is selected
+        boolean r1Selected = r1.isSelected();
+        System.out.println("The radio button Male is selected :"+r1Selected);
+
+        //check if the radio button r1 is displayed
+        boolean r1isDisplayed =  r1.isDisplayed();
+        System.out.println("The radio button Male is displayed :"+r1isDisplayed);
+
+        //check if the radio button r1 is enabled
+        boolean r1isEnabled = r1.isEnabled();
+        System.out.println("The radio button Male is enabled :"+r1isEnabled);
+    }
 }
