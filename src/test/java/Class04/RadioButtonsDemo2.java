05349cf145ca4bdb48f9863c06a20f93b2609966
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 18:13:07 -0700

    Updated Commit

--- src/test/java/Class04/RadioButtonsDemo2.java
+++ src/test/java/Class04/RadioButtonsDemo2.java
@@ -1,6 +1,10 @@
 package Class04;
 
 import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class RadioButtonsDemo2 extends CommonMethods {
     public static void main(String[] args) {
@@ -8,5 +12,18 @@ public class RadioButtonsDemo2 extends CommonMethods {
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
 
+        //get all the radio buttons associated with age group using driver.findElements
+        List<WebElement> ageGroup = driver.findElements(By.xpath("//input[@name='ageGroup']"));
+        //you need to select the option specified by user
+        String option="5 - 15";
+
+        for(WebElement age:ageGroup){
+
+            String value = age.getAttribute("value");
+            if(value.equals(option)){
+                age.click();
+                break;
+            }
+        }
     }
 }
