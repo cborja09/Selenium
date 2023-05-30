7e1ff0a167b80c8db3ea548187426e217bab9542
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 19:11:50 -0700

    Updated Commit

---
+++ src/test/java/Class04/DropDowns.java
@@ -0,0 +1,12 @@
+package Class04;
+
+import Utlis.CommonMethods;
+
+public class DropDowns extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "https://www.facebook.com/";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+    }
+}
