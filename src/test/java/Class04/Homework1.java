d0cc4f992248be2302d2022170a09a2abed6f748
cborja09@icloud.com <cborja09@icloud.com>
2023-05-30 11:27:20 -0700

    Updated Commit

---
+++ src/test/java/Class04/Homework1.java
@@ -0,0 +1,11 @@
+package Class04;
+
+public class Homework1 {
+    /*
+    Go to: http://practice.syntaxtechs.net/basic-select-dropdown-demo.php
+    Select Tuesday, Thursday and Friday one by one
+    */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/Class04/Homework2.java
@@ -0,0 +1,13 @@
+package Class04;
+
+public class Homework2 {
+    /*
+    Go to: facebook.com
+    Click on create account
+    and select your date of birth using select class
+    */
+    public static void main(String[] args) {
+
+    }
+
+}
