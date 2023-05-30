5698988b6204b5392e0447219e65ef0a63f08a70
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 18:25:53 -0700

    Updated Commit

---
+++ src/test/java/Class04/CheckBoxDemo.java
@@ -0,0 +1,11 @@
+package Class04;
+
+import Utlis.CommonMethods;
+
+public class CheckBoxDemo extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://practice.syntaxtechs.net/basic-checkbox-demo.php";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+    }
+}
