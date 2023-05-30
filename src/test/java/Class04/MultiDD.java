619661f22dbb01b0a7757ea8ea009c69352db17c
cborja09@icloud.com <cborja09@icloud.com>
2023-05-29 19:47:53 -0700

    Updated Commit

---
+++ src/test/java/Class04/MultiDD.java
@@ -0,0 +1,11 @@
+package Class04;
+
+import Utlis.CommonMethods;
+
+public class MultiDD extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://practice.syntaxtechs.net/basic-select-dropdown-demo.php";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+    }
+}
