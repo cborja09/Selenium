f46d6ce4309ae93c37e6e68cb19f0ab70b5dbed8
cborja09@icloud.com <cborja09@icloud.com>
2023-05-31 21:52:21 -0700

    Updated Commit

--- src/test/java/Class05/Homework1.java
+++ src/test/java/Class05/Homework1.java
@@ -1,6 +1,9 @@
 package Class05;
 
 import Utlis.CommonMethods;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Homework1 extends CommonMethods {
     /*
@@ -9,7 +12,21 @@ public class Homework1 extends CommonMethods {
     Send keys and accept it
     Note: don't worry if the text you send doesn't appear up in the text box
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String url = "http://practice.syntaxtechs.net/javascript-alert-box-demo.php";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        WebElement alertThird = driver.findElement(By.xpath("//button[@onclick='myPromptFunction()']"));
+        alertThird.click();
+        Thread.sleep(2000);
+
+        Alert confirmAlert3 = driver.switchTo().alert();
+        Thread.sleep(2000);
+        confirmAlert3.sendKeys("Shakka brah");
+        Thread.sleep(2000);
+        confirmAlert3.accept();
+
 
     }
 }
--- src/test/java/Class05/Homework2.java
+++ src/test/java/Class05/Homework2.java
@@ -4,7 +4,7 @@ import Utlis.CommonMethods;
 
 public class Homework2 extends CommonMethods {
     /*
-    Go to:  https://chercher.tech/practice/frames
+    Go to: https://chercher.tech/practice/frames
     Click on check box
     Then select baby cat from drop down
     Then enter text in text box (edited)
