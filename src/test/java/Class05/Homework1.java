bed5d260f74f124ea517382c0ac6f6662da5638d
cborja09@icloud.com <cborja09@icloud.com>
2023-05-31 21:28:28 -0700

    Updated Commit

--- src/test/java/Class05/Homework1.java
+++ src/test/java/Class05/Homework1.java
@@ -1,6 +1,14 @@
 package Class05;
 
-public class Homework1 {
+import Utlis.CommonMethods;
+
+public class Homework1 extends CommonMethods {
+    /*
+    Go to: http://practice.syntaxtechs.net/javascript-alert-box-demo.php
+    Click on the last alert
+    Send keys and accept it
+    Note: don't worry if the text you send doesn't appear up in the text box
+     */
     public static void main(String[] args) {
 
     }
---
+++ src/test/java/Class05/Homework2.java
@@ -0,0 +1,15 @@
+package Class05;
+
+import Utlis.CommonMethods;
+
+public class Homework2 extends CommonMethods {
+    /*
+    Go to:  https://chercher.tech/practice/frames
+    Click on check box
+    Then select baby cat from drop down
+    Then enter text in text box (edited)
+     */
+    public static void main(String[] args) {
+
+    }
+}
