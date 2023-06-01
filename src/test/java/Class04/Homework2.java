0cca442f9cc75994a081678d2548e99cf7d4e19a
cborja09@icloud.com <cborja09@icloud.com>
2023-05-31 21:22:31 -0700

    Updated Commit

--- src/test/java/Class04/Homework2.java
+++ src/test/java/Class04/Homework2.java
@@ -1,12 +1,43 @@
 package Class04;
 
-public class Homework2 {
+import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class Homework2 extends CommonMethods {
     /*
     Go to: facebook.com
     Click on create account
     and select your date of birth using select class
     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://www.facebook.com/";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        WebElement regButton = driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']"));
+        regButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement month = driver.findElement(By.xpath("//select[@id='month']"));
+        Select selMonth = new Select(month);
+        selMonth.selectByVisibleText("Jan");
+        Thread.sleep(2000);
+
+        WebElement day = driver.findElement(By.xpath("//select[@name='birthday_day']"));
+        Select selDay = new Select(day);
+        selDay.selectByValue("1");
+        Thread.sleep(2000);
+
+        WebElement year = driver.findElement(By.xpath("//select[@name='birthday_year']"));
+        Select selYear = new Select(year);
+        selYear.selectByValue("2003");
+        Thread.sleep(2000);
+
+
+
 
     }
 
---
+++ src/test/java/Class05/Homework1.java
@@ -0,0 +1,7 @@
+package Class05;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+    }
+}
