a76646cfeeb1834829bc3924b298689aea541297
cborja09@icloud.com <cborja09@icloud.com>
2023-05-31 20:18:24 -0700

    Updated Commit

--- src/test/java/Class04/Homework1.java
+++ src/test/java/Class04/Homework1.java
@@ -1,17 +1,37 @@
 package Class04;
 
 import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Homework1 extends CommonMethods {
     /*
     Go to: http://practice.syntaxtechs.net/basic-select-dropdown-demo.php
     Select Tuesday, Thursday and Friday one by one
     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String url = "http://practice.syntaxtechs.net/basic-select-dropdown-demo.php";
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
+
+        WebElement day = driver.findElement(By.xpath("//select[@id='select-demo']"));
+
+        Select sel = new Select(day);
+
+        sel.selectByVisibleText("Tuesday");
+
+        Thread.sleep(2000);
+
+        sel.selectByValue("Thursday");
+
+        Thread.sleep(2000);
+
+        sel.selectByIndex(6);
+
+
+
 
     }
 }
