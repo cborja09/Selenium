fa43911dbd9a6eff7d28a3569619b39de9fc48ba
cborja09@icloud.com <cborja09@icloud.com>
2023-06-03 10:57:48 -0700

    Updated Commit

--- src/test/java/Class05/Homework2.java
+++ src/test/java/Class05/Homework2.java
@@ -10,7 +10,7 @@ public class Homework2 extends CommonMethods {
     Go to: https://chercher.tech/practice/frames
     Click on check box
     Then select baby cat from drop down
-    Then enter text in text box (edited)
+    Then enter text in text box
      */
     public static void main(String[] args) throws InterruptedException {
         String url = "https://chercher.tech/practice/frames";
