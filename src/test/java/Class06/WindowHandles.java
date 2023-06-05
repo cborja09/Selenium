212f2c8d3a9241296c096cf54e15059fb64c2734
cborja09@icloud.com <cborja09@icloud.com>
2023-06-05 15:29:29 -0700

    Updated Commit

--- src/test/java/Class06/WindowHandles.java
+++ src/test/java/Class06/WindowHandles.java
@@ -27,11 +27,18 @@ public class WindowHandles extends CommonMethods {
         System.out.println("main page handle is :"+mainPageHandle);
 
         //get all the window handles and print the titles associated with each window handle on the console
+        //get all handles
+        //switch the focus of the driver to Google help page
         Set<String>  allHandles = driver.getWindowHandles();
         for(String handle:allHandles){
             driver.switchTo().window(handle);
             String title = driver.getTitle();
-            System.out.println("the title associated with "+handle+" is :"+title);
+            if (title.equalsIgnoreCase("Google Account Help")){
+                break;
+            }
+            //System.out.println("the title associated with "+handle+" is :"+title);
         }
+        //when the focus is shifted to correct page
+        System.out.println("the current page under focus is : "+driver.getTitle());
     }
 }
