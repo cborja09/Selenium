a9650cb01f98a5a32a48d02d0e8baa27e33bd6cb
cborja09@icloud.com <cborja09@icloud.com>
2023-06-05 16:54:30 -0700

    Updated Commit

---
+++ src/test/java/Class06/ImplicitWait.java
@@ -0,0 +1,22 @@
+package Class06;
+
+import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+public class ImplicitWait extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "https://www.facebook.com/";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //click on create new account
+        WebElement createNewAccountBtn = driver.findElement(By.xpath("//a[text()='Create new account']"));
+        createNewAccountBtn.click();
+
+    }
+}
