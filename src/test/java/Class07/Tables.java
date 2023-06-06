bb2559ddc1fbd197bfd2191c7f25a710914303b9
cborja09@icloud.com <cborja09@icloud.com>
2023-06-06 16:56:13 -0700

    Updated Commit

---
+++ src/test/java/Class07/Tables.java
@@ -0,0 +1,11 @@
+package Class07;
+
+import Utlis.CommonMethods;
+
+public class Tables extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+    }
+}
