7f181959b5adc474d58af3bf79dc915fbb9af93a
cborja09@icloud.com <cborja09@icloud.com>
2023-06-05 19:23:27 -0700

    Updated Commit

---
+++ src/test/java/Class06/ExplicitWait.java
@@ -0,0 +1,32 @@
+package Class06;
+
+import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ExplicitWait extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        //click on enable button after ten seconds and then click on the button
+        //assumption we don't know what is the time for it to get enabled
+
+        //click on the enable button
+        WebElement button1 = driver.findElement(By.xpath("//button[@id='enable-button']"));
+        button1.click();
+        //declare the wait
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Button']")));
+        //click on the button
+        WebElement eBtn = driver.findElement(By.xpath("//button[text()='Button']"));
+        eBtn.click();
+    }
+}
