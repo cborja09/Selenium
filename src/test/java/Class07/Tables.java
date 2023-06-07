b58927aac32c13525f5efc7fdb8c1c0e811b4ee9
cborja09@icloud.com <cborja09@icloud.com>
2023-06-06 17:05:33 -0700

    Updated Commit

--- src/test/java/Class07/Tables.java
+++ src/test/java/Class07/Tables.java
@@ -1,11 +1,18 @@
 package Class07;
 
 import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Tables extends CommonMethods {
     public static void main(String[] args) {
         String url = "https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html";
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
+
+        //print the whole table on the console
+        WebElement wholeTable = driver.findElement(By.xpath("//table[@id='customers']"));
+        String allText = wholeTable.getText();
+        System.out.println(allText);
     }
 }
