43a9214e5ffae8d64f22f53897327fee6a21c54a
cborja09@icloud.com <cborja09@icloud.com>
2023-06-06 17:12:00 -0700

    Updated Commit

--- src/test/java/Class07/Tables.java
+++ src/test/java/Class07/Tables.java
@@ -4,6 +4,8 @@ import Utlis.CommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class Tables extends CommonMethods {
     public static void main(String[] args) {
         String url = "https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html";
@@ -14,5 +16,14 @@ public class Tables extends CommonMethods {
         WebElement wholeTable = driver.findElement(By.xpath("//table[@id='customers']"));
         String allText = wholeTable.getText();
         System.out.println(allText);
+
+        System.out.println("***********************");
+
+        //print all rows of the table
+        List<WebElement> allRows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
+        for (WebElement row:allRows){
+            String rowText = row.getText();
+            System.out.println(rowText);
+        }
     }
 }
