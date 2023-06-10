5c210c43bac2f6b9ff157f774bbb1ff5184562ad
cborja09@icloud.com <cborja09@icloud.com>
2023-06-10 16:59:36 -0700

    Updated Commit

--- src/test/java/Class07/Tables.java
+++ src/test/java/Class07/Tables.java
@@ -12,19 +12,20 @@ public class Tables extends CommonMethods {
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
 
-        //print the whole table on the console
+        //Get the whole table
         WebElement wholeTable = driver.findElement(By.xpath("//table[@id='customers']"));
         String allText = wholeTable.getText();
         System.out.println(allText);
 
         System.out.println("-----------------------------");
 
-        //print all rows of the table
+        //Print all the rows of the table
         List<WebElement> allRows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
         for (WebElement row:allRows){
             String rowText = row.getText();
             System.out.println(rowText);
         }
+
         System.out.println("-----------------------------");
         //print each and every data in the table
         List<WebElement> allCellData = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr/td"));
