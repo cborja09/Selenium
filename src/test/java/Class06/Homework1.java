a63b929b804b7cd3b9eb3cd86dc129f31fbd7722
cborja09@icloud.com <cborja09@icloud.com>
2023-06-10 10:48:08 -0700

    Updated Commit

--- src/test/java/Class06/Homework1.java
+++ src/test/java/Class06/Homework1.java
@@ -4,6 +4,29 @@ package Class06;
 //Click on start
 //Print the text "welcome syntax technologies" on console
 
-public class Homework1 {
+import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
+public class Homework1 extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://practice.syntaxtechs.net/dynamic-elements-loading.php";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        //Click on start button
+        WebElement startBtn = driver.findElement(By.xpath("//button[@id='startButton']"));
+        startBtn.click();
+
+        //Print the text "welcome to syntax technologies"
+        WebElement textMessage = driver.findElement(By.xpath("//h4[text()='Welcome Syntax Technologies']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.textToBePresentInElement(textMessage, "Welcome Syntax Technologies"));
+        System.out.println("The text after clicking on the start button is : "+ textMessage.getText());
+        driver.close();
+    }
 }
--- src/test/java/Class06/Homework3.java
+++
@@ -1,5 +0,0 @@
-package Class06;
-//Develop a function in common methods to select from dropdown(single select)
-
-public class Homework3 {
-}
