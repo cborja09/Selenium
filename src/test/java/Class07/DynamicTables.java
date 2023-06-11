17ee99677cb0a3ca93534c4cc1241723611b9950
cborja09@icloud.com <cborja09@icloud.com>
2023-06-10 19:13:12 -0700

    Updated Commit

--- src/test/java/Class07/DynamicTables.java
+++ src/test/java/Class07/DynamicTables.java
@@ -3,7 +3,6 @@ package Class07;
 import Utlis.CommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
 import java.util.List;
 
 public class DynamicTables extends CommonMethods {
