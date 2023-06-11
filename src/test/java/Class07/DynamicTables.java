8cc8718e5907c94090f48f59425a0104bf2e94ca
cborja09@icloud.com <cborja09@icloud.com>
2023-06-10 18:05:18 -0700

    Updated Commit

--- src/test/java/Class07/DynamicTables.java
+++ src/test/java/Class07/DynamicTables.java
@@ -1,11 +1,21 @@
 package Class07;
 
 import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class DynamicTables extends CommonMethods {
     public static void main(String[] args) {
         String url = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login";
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
+
+        //Login into the website
+        WebElement userName= driver.findElement(By.xpath("//input[@id='txtUsername']"));
+        userName.sendKeys("Admin");
+        WebElement password=driver.findElement(By.xpath("//input[@id='txtPassword']"));
+        password.sendKeys("Hum@nhrm123");
+        WebElement loginBtn=driver.findElement(By.xpath("//input[@id='btnLogin']"));
+        loginBtn.click();
     }
 }
