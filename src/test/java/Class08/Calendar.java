a2707def794892b23bb203a4ca73dc9568b71569
cborja09@icloud.com <cborja09@icloud.com>
2023-06-11 00:00:21 -0700

    Updated Commit

--- src/test/java/Class08/Calendar.java
+++ src/test/java/Class08/Calendar.java
@@ -4,6 +4,7 @@ import Utlis.CommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import java.util.List;
 
 public class Calendar extends CommonMethods {
     public static void main(String[] args) {
@@ -16,13 +17,20 @@ public class Calendar extends CommonMethods {
         calendar.click();
 
         //Select the date August 4 2023
-        //get the month and check if it is the desired one
         boolean monthFound = false;
         while (!monthFound) {
+            //get the month and check if it is the desired one
             WebElement month = driver.findElement(By.xpath("(//span[@class='ui-datepicker-month'])[1]"));
             String currentMonth = month.getText();
             if (currentMonth.equals("August")) {
                 System.out.println("You are on the right month");
+                List<WebElement> alldates = driver.findElements(By.xpath("//table/tbody/tr/td"));
+                for (WebElement date:alldates){
+                     String currentDate = date.getText();
+                     if (currentDate.equals("4")) {
+                         date.click();
+                     }
+                }
                 monthFound = true;
             } else {
                 WebElement nextBtn = driver.findElement(By.xpath("(//a[@class='ui-datepicker-next ui-corner-all'])")); //Click on the next page
