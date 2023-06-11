7168bd2e3fd3a0dc98054bec0cbaa047b24eff6a
cborja09@icloud.com <cborja09@icloud.com>
2023-06-10 20:20:52 -0700

    Updated Commit

---
+++ src/test/java/Class08/Homework.java
@@ -0,0 +1,11 @@
+package Class08;
+//Go to: syntax hrms
+//Login
+//Click on recruitment
+//Select a date on the calendar
+
+public class Homework {
+    public static void main(String[] args) {
+
+    }
+}
