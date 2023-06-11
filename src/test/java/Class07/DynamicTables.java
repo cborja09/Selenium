5ab03339524a4d6bf4ef1d360f663843f488c9ba
cborja09@icloud.com <cborja09@icloud.com>
2023-06-10 17:49:21 -0700

    Updated Commit

--- src/test/java/Class07/DynamicTables.java
+++ src/test/java/Class07/DynamicTables.java
@@ -1,7 +1,11 @@
 package Class07;
 
-public class DynamicTables {
-    public static void main(String[] args) {
+import Utlis.CommonMethods;
 
+public class DynamicTables extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
     }
 }
