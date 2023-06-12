31d0bd3fa5f2df86aa6b92d8fc45984d85799a88
cborja09@icloud.com <cborja09@icloud.com>
2023-06-11 19:54:02 -0700

    Updated Commit

--- src/test/java/Class09/ActionClassDemo2.java
+++ src/test/java/Class09/ActionClassDemo2.java
@@ -1,12 +1,22 @@
 package Class09;
 
 import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class ActionClassDemo2 extends CommonMethods {
     public static void main(String[] args) {
         String url = "https://jqueryui.com/droppable/";
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
+
+        //Find the draggable WebElement
+        WebElement drag = driver.findElement(By.id("draggable"));
+
+        //Find the WebElement where you want to drop
+        WebElement drop = driver.findElement(By.id("droppable"));
+
+
 
     }
 }
