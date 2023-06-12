8beed494924817b2f2da8532210e0a1dbb9a610a
cborja09@icloud.com <cborja09@icloud.com>
2023-06-11 19:36:23 -0700

    Updated Commit

---
+++ src/test/java/Class09/ActionClassDemo2.java
@@ -0,0 +1,12 @@
+package Class09;
+
+import Utlis.CommonMethods;
+
+public class ActionClassDemo2 extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "https://jqueryui.com/droppable/";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+    }
+}
