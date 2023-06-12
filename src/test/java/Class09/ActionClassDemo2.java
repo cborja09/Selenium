37a7b065df75aaf8c5b4e1286ccf0cd7ad179960
cborja09@icloud.com <cborja09@icloud.com>
2023-06-11 20:05:40 -0700

    Updated Commit

--- src/test/java/Class09/ActionClassDemo2.java
+++ src/test/java/Class09/ActionClassDemo2.java
@@ -3,6 +3,7 @@ package Class09;
 import Utlis.CommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class ActionClassDemo2 extends CommonMethods {
     public static void main(String[] args) {
@@ -10,13 +11,18 @@ public class ActionClassDemo2 extends CommonMethods {
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
 
+        //Element is inside an iframe, so we must switch focus
+        driver.switchTo().frame(0);
+
         //Find the draggable WebElement
         WebElement drag = driver.findElement(By.id("draggable"));
 
         //Find the WebElement where you want to drop
         WebElement drop = driver.findElement(By.id("droppable"));
 
-
+        //Action Class
+        Actions action = new Actions(driver);
+        action.dragAndDrop(drag,drop).perform();
 
     }
 }
