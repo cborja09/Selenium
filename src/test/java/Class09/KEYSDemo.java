d61a4b8efab9229c410efc694d213c62f6b198c9
cborja09@icloud.com <cborja09@icloud.com>
2023-06-13 17:29:42 -0700

    Updated Commit

---
+++ src/test/java/Class09/KEYSDemo.java
@@ -0,0 +1,18 @@
+package Class09;
+
+import Utlis.CommonMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class KEYSDemo extends CommonMethods {
+    public static void main(String[] args) {
+        String url = "http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        //Locate the text box username
+        WebElement userName = driver.findElement(By.id("ctl00_MainContent_username"));
+        userName.sendKeys("Tester",Keys.TAB);
+    }
+}
