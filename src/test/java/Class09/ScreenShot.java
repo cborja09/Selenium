50307259c638075b2ba03337b5ad5d1f47f38765
cborja09@icloud.com <cborja09@icloud.com>
2023-06-13 22:03:10 -0700

    Updated Commit

--- src/test/java/Class09/ScreenShot.java
+++ src/test/java/Class09/ScreenShot.java
@@ -1,12 +1,14 @@
 package Class09;
 
 import Utlis.CommonMethods;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ScreenShot extends CommonMethods {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String url = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login";
         String browser = "chrome";
         openBrowserAndLaunchApplication(url, browser);
@@ -17,5 +19,17 @@ public class ScreenShot extends CommonMethods {
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();",loginBtn);
+
+        //Take the screenshot
+
+        //Declare the instance
+        TakesScreenshot ts = (TakesScreenshot) driver;
+
+        //Take screenshot as output type FILE
+        File screenShot = ts.getScreenshotAs(OutputType.FILE);
+
+        //Save it in your computer
+        FileUtils.copyFile(screenShot,new File("/Users/coreyborja/IdeaProjects/SyntaxSdetBatch16Basic/screenshot/testSyntax.png"));
+
     }
 }
