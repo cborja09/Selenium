acbcaa3227d51892792f20019e95edfe7b938376
cborja09@icloud.com <cborja09@icloud.com>
2023-06-13 18:55:22 -0700

    Updated Commit

---
+++ src/test/java/Class09/JSExecutorDemo2.java
@@ -0,0 +1,24 @@
+package Class09;
+
+import Utlis.CommonMethods;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class JSExecutorDemo2 extends CommonMethods {
+    public static void main(String[] args) throws InterruptedException {
+        String url = "http://amazon.com";
+        String browser = "chrome";
+        openBrowserAndLaunchApplication(url, browser);
+
+        //Perform a scroll operation
+        //Using Java Script
+
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+        //Scroll down
+        js.executeScript("window.scrollBy(0, 500)");
+        //Sleep for observation
+        Thread.sleep(3000);
+        //Scroll up
+        js.executeScript("window.scrollBy(0, -500)");
+
+    }
+}
